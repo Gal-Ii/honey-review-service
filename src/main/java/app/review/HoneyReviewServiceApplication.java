64a7aaa1d7bc58f7917a0de192828c184586cb8d
Galina65 <galina.georgieva.uch@abv.bg>
2026-07-26 02:30:36 +0300

    chore: initialize review microservice

---
+++ src/main/java/app/review/HoneyReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package app.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/review/HoneyReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
